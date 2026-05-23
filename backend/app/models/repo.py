0e9fe8243068dc3e62d9ccdce737f2c4b7203757
Devashish Gogoi <hellonat3456@gmail.com>
2026-05-23 16:35:54 +0530

    Update repo.py

--- backend/app/models/repo.py
+++ backend/app/models/repo.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
+from sqlalchemy import Column, Integer, BigInteger, String, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.core.database import Base
